9d0bf8fbd7e3d68101f8b259e134a3623112f85b
Mustafa Saad <m.saad@gama-ksa.com>
2024-09-23 20:16:32 +0300

    Start search implementation

--- starter/src/SearchBooks.js
+++ starter/src/SearchBooks.js
@@ -1,5 +1,15 @@
 import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import { useState } from "react";
 const SearchBooks = () => {
+  const [books, setBooks] = useState([]);
+  const SearchBooks = (query) => {
+    console.log(query);
+    BooksAPI.search(query).then((response) => {
+      console.log(response);
+      setBooks(response);
+    });
+  };
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -7,11 +17,25 @@ const SearchBooks = () => {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input
+            onChange={(event) => {
+              SearchBooks(event.target.value);
+            }}
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+          />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {books.map((book) => {
+            return (
+              <li key={book.id}>
+                <h1>{book.title}</h1>
+              </li>
+            );
+          })}
+        </ol>
       </div>
     </div>
   );
